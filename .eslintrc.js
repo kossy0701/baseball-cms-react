c0d50e25f09618af2adc8e64ce6379ede136c320
kossy0701 <0421kossy0421@gmail.com>
2020-06-07 17:36:23 +0900

    set prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,31 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    'jest/globals': true
+    "jest/globals": true,
   },
   extends: [
-    'plugin:react/recommended',
-    'standard'
+    "plugin:react/recommended",
+    "standard",
+    "plugin:prettier/recommended",
   ],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    'jest'
-  ],
+  plugins: ["react", "jest", "prettier"],
   rules: {
+    "prettier/prettier": "error",
   },
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
-}
+      version: "detect",
+    },
+  },
+};
